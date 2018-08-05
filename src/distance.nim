dffb6d9d3a83b7d565d071c3f62a8327f0d75984
Philip Rideout <philiprideout@gmail.com>
2018-08-05 15:25:05 -0700

    Faster SeqView

--- src/distance.nim
+++ src/distance.nim
@@ -1,24 +1,26 @@
+#!/usr/bin/env nim c -d:release --boundChecks:off --verbosity:0 --run
+
 import grid
 
 const INF: float32 = 1e20
 
 type Viewf = object
-  data: ref seq[float32]
+  data: ptr seq[float32]
   offset: int
 
 type Viewi = object
-  data: ref seq[uint16]
+  data: ptr seq[uint16]
   offset: int
 
-proc box[T](x: T): ref T = new(result); result[] = x
-proc set(sv: Viewf, i: int, val: float32): void = sv.data[sv.offset + i] = val
-proc get(sv: Viewf, i: int): float32 = sv.data[sv.offset + i]
+# TODO: instead of get / set, overload `[]` and `[]=`
+proc set(sv: var Viewf, i: int, val: float32): void = sv.data[sv.offset + i] = val
+proc get(sv: var Viewf, i: int): float32 = sv.data[sv.offset + i]
 proc get(g: Grid, x, y: int): float32 = g.data[y * g.width + x]
 proc set(g: Grid, x, y: int, v: float32): void = g.data[y * g.width + x] = v
 
 proc createEdt*(grid: Grid): Grid =
     new(result)
-    let
+    var
         width = grid.width
         height = grid.height
         npixels = width * height
@@ -37,10 +39,10 @@ proc createEdt*(grid: Grid): Grid =
     echo "Processing Columns..."
     for x in 0..<width:
         var
-            f = Viewf(data: box(ff.data), offset: height * x)
-            d = Viewf(data: box(dd.data), offset: height * x)
-            z = Viewf(data: box(zz.data), offset: (height + 1) * x)
-            w = Viewi(data: box(ww), offset: height * x)
+            f = Viewf(data: addr ff.data, offset: height * x)
+            d = Viewf(data: addr dd.data, offset: height * x)
+            z = Viewf(data: addr zz.data, offset: (height + 1) * x)
+            w = Viewi(data: addr ww, offset: height * x)
         for y in 0..<height:
             f.set(y, result.get(x, y))
         # foo()
@@ -50,10 +52,10 @@ proc createEdt*(grid: Grid): Grid =
     echo "Processing Rows..."
     for y in 0..<height:
         var
-            f = Viewf(data: box(ff.data), offset: width * y)
-            d = Viewf(data: box(dd.data), offset: width * y)
-            z = Viewf(data: box(zz.data), offset: (width + 1) * y)
-            w = Viewi(data: box(ww), offset: width * y)
+            f = Viewf(data: addr ff.data, offset: width * y)
+            d = Viewf(data: addr dd.data, offset: width * y)
+            z = Viewf(data: addr zz.data, offset: (width + 1) * y)
+            w = Viewi(data: addr ww, offset: width * y)
         for x in 0..<width:
             f.set(x, result.get(x, y))
         # foo()
@@ -61,6 +63,8 @@ proc createEdt*(grid: Grid): Grid =
             result.set(x, y, d.get(x))
 
 if isMainModule:
-    let seq1 = @[1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f]
-    let sv = Viewf(data: box(seq1), offset: 2)
-    discard sv
+    var seq1 = @[1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f]
+    var sv = Viewf(data: addr seq1, offset: 2)
+    sv.data[1] = 5.0f
+    echo sv.data[0]
+    echo sv.data[1]
